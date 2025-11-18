81f1ec7a8e11d9a2be156e6ea084c2422050e9a9
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2025-11-18 12:56:50 -0400

    preparando la aplicacion

---
+++ FormularioPersonas/Entidades/Personas.cs
@@ -0,0 +1,11 @@
+﻿namespace FormularioPersonas.Entidades
+{
+    public class Personas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Cedula { get; set; } = null!;
+       
+    }
+}
--- FormularioPersonas/Program.cs
+++ FormularioPersonas/Program.cs
@@ -1,6 +1,56 @@
+using FormularioPersonas.Entidades;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+var origenesPermitidos = builder.Configuration.GetValue<string>("origenesPermitidos")!;
+//inicio de area de los servicios
+builder.Services.AddCors(opciones => 
+{
+    opciones.AddDefaultPolicy
+    (configuracion =>
+    {
+        configuracion.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+    });
+
+    opciones.AddPolicy("libre", configuracion =>
+    {
+        configuracion.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+//fin de area de los servicios
 var app = builder.Build();
+//inicio de los middleware
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
 
+app.MapGet("/",[EnableCors(policyName:"libre")] () => "Hello World!");
+app.MapGet("/personas", () =>
+{
+    var personas = new List<Personas> 
+    { 
+        new Personas
+        {
+            Id = 1,
+            Nombre ="Ramon",
+            Apellido ="Santos",
+            Cedula="402-255513-38"
+            
+        },
+         new Personas
+        {
+            Id = 2,
+            Nombre ="Rosa",
+            Apellido ="Martinez",
+            Cedula="402-255513-38",
+            
+
+        }
+
+    };
+    return personas;
+});
+
+//fin del area de los middleware
 app.Run();
